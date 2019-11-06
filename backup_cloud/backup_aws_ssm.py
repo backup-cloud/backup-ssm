e63677ea325e519c79f4b59a3e0589a047e155b7
Michael De La Rue <mdlr@paddle.com>
2019-11-06 00:04:06 +0000

    passing backup/restore test as python function

--- backup_cloud/backup_aws_ssm.py
+++ backup_cloud/backup_aws_ssm.py
@@ -1,6 +1,19 @@
+from backup_cloud.aws_ssm_dict import aws_ssm_dict
+import json
+
+
 def backup_to_file(filename):
-    pass
+    ssm_dict = aws_ssm_dict()
+    contents = {}
+    for i in ssm_dict.keys():
+        contents[i] = ssm_dict[i]
+    with open(filename, "w") as f:
+        json.dump(contents, f)
 
 
 def restore_from_file(filename):
-    pass
+    ssm_dict = aws_ssm_dict()
+    with open(filename) as f:
+        contents = json.load(f)
+    for i in contents.keys():
+        ssm_dict[i] = contents[i]
